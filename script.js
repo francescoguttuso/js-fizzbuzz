33445537863ae6c105edcd3a97e95a9a1d74460e
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-12 23:57:55 +0200

    Add fizz+buzz

---
+++ script.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 100; i++) {
+    if (i % 3 === 0) {
+        // Al posto del numero, FIZZ
+        console.log("FIZZ");
+    } if (i % 5 === 0) {
+        // Al posto del numero, BUZZ
+        console.log("BUZZ");
+    }
+
+    else {
+        // Altrimenti stampiamo il numero normale
+        console.log(i);
+    }
+}
+
+
+
