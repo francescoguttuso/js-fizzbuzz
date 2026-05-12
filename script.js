f1069eec2dd31d5fbe35f8f3aede1d04ff5bce4f
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-13 00:18:50 +0200

    fix problem

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-for (let i = 0; i < 100; i++) {
-    if (i % 3 === 0) {
-        // Al posto del numero, FIZZ
-        console.log("FIZZ");
-    } if (i % 5 === 0) {
-        // Al posto del numero, BUZZ
-        console.log("BUZZ");
-    } if (i % 3 === 0 && i % 5 === 0) {
+for (let i = 0; i < 101; i++) {
+    if (i === 0) {
+        // Se il numero è 0, scrivo zero
+        console.log(i);
+    }
+    else if (i % 3 === 0 && i % 5 === 0) {
         // Al posto del numero, FIZZBUZZ
         console.log("FIZZBUZZ");
     }
-
-
+    else if (i % 3 === 0) {
+        // Al posto del numero, FIZZ
+        console.log("FIZZ");
+    }
+    else if (i % 5 === 0) {
+        // Al posto del numero, BUZZ
+        console.log("BUZZ");
+    }
     else {
         // Altrimenti stampiamo il numero normale
         console.log(i);
